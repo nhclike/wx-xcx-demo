def888a2be69fe6dd4a21b4ebda8261d27cc967e
nhclike <2471814041@qq.com>
2019-04-11 09:02:52 +0800

    配置修改

--- pages/index/index.js
+++ pages/index/index.js
@@ -78,6 +78,15 @@ Page({
       url: './../detail/detail',
     })
   },
+  upper(e) {
+    console.log(e)
+  },
+  lower(e) {
+    console.log(e)
+  },
+  scroll(e) {
+    console.log(e)
+  },
   onLoad:function(){
     
   },
